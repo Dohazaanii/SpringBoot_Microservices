e926f98aea216bcfc7c26ce6069c6ca2ca42de3a
Vextended <esserhaniaymane@gmail.com>
2025-12-11 12:16:17 +0100

    repoo

--- gestion_vente/src/main/java/com/tp/gestion_vente/repository/UsersRepository.java
+++ gestion_vente/src/main/java/com/tp/gestion_vente/repository/UsersRepository.java
@@ -2,7 +2,9 @@ package com.tp.gestion_vente.repository;
 
 import com.tp.gestion_vente.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsersRepository  extends JpaRepository<Users, Long> {
-    public Users findByLoginAndPass(String login, String pass);
+    Users findByLoginAndPass(String login, String pass);
 }
