c36dc8931e9c3d96775ba09567209c9cfcfc4bb4
Vextended <esserhaniaymane@gmail.com>
2025-12-11 11:48:23 +0100

    Users entity

---
+++ gestion_vente/src/main/java/com/tp/gestion_vente/entity/Users.java
@@ -0,0 +1,10 @@
+package com.tp.gestion_vente.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = )
+}
